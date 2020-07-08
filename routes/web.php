58293304c67f83f37630143f041008cbc2172efe
sieroniekuggy <sieroniekuggy@gmail.com>
2020-07-08 14:36:24 +0800

    sending mails

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Mail\WelcomeMail;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return new WelcomeMail();
+});
 
 Route::get('/about', 'HelloController@about');
 
@@ -11,7 +15,7 @@ Route::post('/service', 'ServiceController@store');
 Route::get('/customers', 'CustomerController@index');
 Route::get('/customers/create', 'CustomerController@create');
 Route::post('/customers', 'CustomerController@store');
-Route::get('/customers/{customer}', 'CustomerController@show'); 
-Route::get('/customers/{customer}/edit', 'CustomerController@edit'); 
-Route::patch('/customers/{customer}', 'CustomerController@update'); 
-Route::delete('/customers/{customer}', 'CustomerController@destroy'); 
+Route::get('/customers/{customer}', 'CustomerController@show');
+Route::get('/customers/{customer}/edit', 'CustomerController@edit');
+Route::patch('/customers/{customer}', 'CustomerController@update');
+Route::delete('/customers/{customer}', 'CustomerController@destroy');
